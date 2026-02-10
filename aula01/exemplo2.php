feacbb3eef403132d1533aa2f471ae24a9dbdaaa
helolrodrigues <161508995+helolrodrigues@users.noreply.github.com>
2026-02-10 17:32:34 -0300

    Add files via upload

---
+++ aula01/exemplo2.php
@@ -0,0 +1,18 @@
+<?php
+$nome = "Ana";
+$idade = "15";
+$preco = "10.50";
+$ativo = "true";
+echo "Nome:" . $nome . "<br>";
+echo "Idade:" . $idade . "<br>";
+echo "Preço:" . $preco. "<br>";
+echo "Status:" . ($ativo ? "Ativo" : "Inativo");
+
+
+
+
+
+
+
+
+?>

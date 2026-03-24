a32313c056fdd7fab11f05ca003f481b55ae6509
helolrodrigues <161508995+helolrodrigues@users.noreply.github.com>
2026-03-24 17:50:39 -0300

    Add files via upload

---
+++ exer_aula04/exercicio01.php
@@ -0,0 +1,9 @@
+<?php
+
+
+function nome($nome){
+    echo "Seja bem vinda $nome!";
+}
+
+echo nome ("Helo");
+?>
---
+++ exer_aula04/exercicio02.php
@@ -0,0 +1,13 @@
+<?php
+
+function diferença ($x,$y){
+    $diferença = $x - $y;
+    return $diferença;
+}
+
+
+$num1 = 20;
+$num2 = 14;
+
+echo diferença ($num1,$num2);
+?>
